e9ce7961a0446fbb704bd59dc104ac47770e1f8d
naveen332003 <naveen332003@gmail.com>
2023-02-01 19:31:04 +0530

    all

--- welcometozohocorporation.c
+++ welcometozohocorporation.c
@@ -1,3 +1,19 @@
+/*Question:
+Save the string “WELCOMETOZOHOCORPORATION” in a two dimensional array and search for substring
+ like “too” in the two dimensional string both from left to right and from top to bottom.
+
+w	e	L	C	O
+M	E	T	O	Z
+O	H	O	C	O
+R	P	O	R	A
+T	I	O	n	  
+
+And print the start and ending index as
+
+Start index : <1,2>
+
+End index: <3, 2>
+*/
 #include<stdio.h>
 void main(){
 	int i,j,c=0,flag=0;
